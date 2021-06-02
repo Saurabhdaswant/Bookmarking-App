13109c9c09847546f7ce31338b593cc91b5d3afd
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-06-02 19:57:28 +0530

    storing bookmark in bookmark arr

--- script.js
+++ script.js
@@ -24,17 +24,40 @@ function createBookmark(e) {
   e.preventDefault();
 
   //add a new bookmark (item === bookmark) to the bookmarks (array)
-  //save that bookmarks list to  the local storage
   const userInput = bookmarkFormInput.value;
-  const bookmark = document.createElement("a");
+  const bookmark = {
+    title: userInput,
+  };
+  bookmarks.push(bookmark);
+  fillbookmarksList(bookmarks);
+
+  console.table(bookmarks);
+  //save that bookmarks list to  the local storage
+
+  // const userInput = bookmarkFormInput.value;
+  // const bookmark = document.createElement("a");
   //the bookmark === a tag  which will born after submit
-  bookmark.className = "mainContent";
-  bookmark.textContent = userInput;
-  bookmark.href = "#";
-  bookmark.target = "_blank";
-  bookmarkList.appendChild(bookmark);
+  // bookmark.className = "mainContent";
+  // bookmark.textContent = userInput;
+  // bookmark.href = "#";
+  // bookmark.target = "_blank";
+  // bookmarkList.appendChild(bookmark);
 
   bookmarkForm.reset();
+}
+
+function fillbookmarksList(bookmarks = []) {
+  let bookmarksHtml = "";
+  for (let i = 0; i < bookmarks.length; i++) {
+    bookmarksHtml += `
+    <a href = "#" class = "mainContent">
+    ${bookmarks[i].title}
+    </a> 
+    `;
+  }
+
+  console.log(bookmarksHtml);
+  bookmarkList.innerHTML = bookmarksHtml;
 }
 
 bookmarkForm.addEventListener("submit", createBookmark);
